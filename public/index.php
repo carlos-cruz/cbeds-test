9346a66fc7635e7c137c50dc7bb9f7e24f7a6d83
Carlos Cruz <jccs24@gmail.com>
2019-05-12 10:12:23 -0700

    Public UI and application start

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+include '../app/DB.class.php';
+include '../app/Interval.php';
+include '../app/Request.php';
+include '../app/Route.php';
+include '../app/Api.php';
+
+include '../app/App.php';
+
+
+$app = App::getInstance();
+
+echo $app->run();
+
+?>
