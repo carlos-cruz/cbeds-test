e3b851d4c0c7b1b71378cf39d823be567150d1f2
Carlos Cruz <jccs24@gmail.com>
2019-05-15 10:55:07 -0700

    Include new classes

--- public/index.php
+++ public/index.php
@@ -2,11 +2,20 @@
 
 include '../app/DB.class.php';
 include '../app/Interval.php';
+
+include '../app/Response.php';
+include '../app/JsonResponse.php';
+include '../app/HtmlResponse.php';
+include '../app/ViewAbstract.php';
+include '../app/JsonView.php';
+include '../app/HtmlView.php';
+
 include '../app/Request.php';
 include '../app/Route.php';
 include '../app/Router.php';
-include '../app/Response.php';
+include '../app/RestfullControllerInterface.php';
 include '../app/Api.php';
+
 
 include '../app/App.php';
 
